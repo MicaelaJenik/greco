352d8e4a41b5abdaf0a44ac8d789b070d204d54f
Micaela Jenik <micaela.jenik@gmail.com>
2021-07-01 10:45:55 -0300

    corrections with julian

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'bio', to: 'pages#bio'
+  get 'welcome', to: 'pages#welcome'
   resources :artworks
   resources :phases
   resources :shows
