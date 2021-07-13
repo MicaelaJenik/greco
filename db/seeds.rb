7b6db819bdf0a99dc0f44055d42cb25c52039239
Micaela Jenik <micaela.jenik@gmail.com>
2021-07-13 11:58:37 -0300

    trying to fix forest problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Phase.delete_all
 Live.delete_all
 
 puts 'Creating El Nascimiento'
-@elnascimiento = Artwork.create(title: 'El Nascimiento', year: 1950, height: 10, width: 10, depth: 10, technique: 'Tinta sobre papel', about: 'Pintado en Buenos Aires', description: 'Bajo la influencia de alucinógenos', period: 'Informalismo', series: 'Tinta')
+# @elnascimiento = Artwork.create(title: 'El Nascimiento', year: 1950, height: 10, width: 10, depth: 10, technique: 'Tinta sobre papel', about: 'Pintado en Buenos Aires', description: 'Bajo la influencia de alucinógenos', period: 'Informalismo', series: 'Tinta')
 
 puts 'Creating Cristo del 63'
 @cristo63 = Show.create(title: 'Cristo del 63', subtitle: 'Una performance unica', year: 1963, place: 'Galeria', city: 'Roma', country: 'Italia', about: 'Paso un dia en la carcel', details: 'Luego se tuvo que fugar de Italia')
