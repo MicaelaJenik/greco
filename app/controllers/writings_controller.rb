db8dec6ff039bb023378b325b985cc63479cad35
Micaela Jenik <micaela.jenik@gmail.com>
2021-07-16 15:34:19 -0300

    adding design

--- app/controllers/writings_controller.rb
+++ app/controllers/writings_controller.rb
@@ -1,7 +1,15 @@
 class WritingsController < ApplicationController
+  before_action :set_writings
+
   def index
   end
 
   def show
+  end
+
+  private
+
+  def set_writings
+    @writings = Writing.all
   end
 end
